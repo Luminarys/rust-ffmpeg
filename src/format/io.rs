6fe01412f02a441cba986f8213bd2424b9c3cbf7
Luminarys <postmaster@gensok.io>
2016-11-29 17:18:57 -0800

    Add custom cleanup function for AVIOContext

--- src/format/io.rs
+++ src/format/io.rs
@@ -2,6 +2,7 @@ use ffi::*;
 use libc::{c_uchar, c_void, uint8_t, c_int, int64_t};
 
 pub struct Context {
+    cleanup: fn(*mut c_void),
     ptr: *mut AVIOContext,
 }
 
@@ -21,13 +22,16 @@ extern "C" fn default_seek(_opaque: *mut c_void, _offset: int64_t, _whence: c_in
     0
 }
 
+fn default_cleanup(_opaque: *mut c_void) {}
+
 impl Context {
     pub fn new<T>(buffer_size: usize,
                   writeable: bool,
                   data: T,
                   read_packet: Option<extern "C" fn(*mut c_void, *mut uint8_t, c_int) -> c_int>,
                   write_packet: Option<extern "C" fn(*mut c_void, *mut uint8_t, c_int) -> c_int>,
-                  seek: Option<extern "C" fn(*mut c_void, int64_t, c_int) -> int64_t>)
+                  seek: Option<extern "C" fn(*mut c_void, int64_t, c_int) -> int64_t>,
+                  cleanup: Option<fn(*mut c_void)>)
                   -> Context {
         unsafe {
             let buffer = av_malloc(buffer_size) as *mut c_uchar;
@@ -43,6 +47,10 @@ impl Context {
                 Some(f) => f,
                 None => default_seek,
             };
+            let cf = match cleanup {
+                Some(f) => f,
+                None => default_cleanup,
+            };
             let w = if writeable {
                 1
             } else {
@@ -56,15 +64,24 @@ impl Context {
                                          rf,
                                          wf,
                                          sf);
-            Context { ptr: ptr }
+            Context {
+                ptr: ptr,
+                cleanup: cf,
+            }
         }
     }
 
-	pub unsafe fn as_ptr(&self) -> *const AVIOContext {
-		self.ptr as *const _
-	}
+    pub unsafe fn as_ptr(&self) -> *const AVIOContext {
+        self.ptr as *const _
+    }
 
-	pub unsafe fn as_mut_ptr(&mut self) -> *mut AVIOContext {
-		self.ptr
-	}
+    pub unsafe fn as_mut_ptr(&mut self) -> *mut AVIOContext {
+        self.ptr
+    }
+}
+
+impl Drop for Context {
+    fn drop(&mut self) {
+        unsafe { (self.cleanup)((*self.ptr).opaque) }
+    }
 }
